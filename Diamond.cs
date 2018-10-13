ab486e554be1edea2e70f32ec32edb2e9b6d8eac
Romain Deneau <rdeneau@yahoo.fr>
2018-10-13 21:50:21 +0200

    Initialize project with first test red (diamond C)

---
+++ Diamond.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace DiamondKata
+{
+    public class Diamond
+    {
+        public IEnumerable<string> Of(string letter)
+        {
+            yield break;
+        }
+    }
+}
---
+++ DiamondTest.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using Xunit;
+
+namespace DiamondKata
+{
+    public class DiamondTest
+    {
+        private readonly Diamond diamond = new Diamond();
+
+        [Fact(Skip = "Pending Diamond_C")]
+        public void Diamond_A()
+        {
+            var diamondA = diamond.Of("A");
+            diamondA.Should().ContainInOrder("A");
+        }
+
+        [Fact(Skip = "Pending Diamond_C")]
+        public void Diamond_B()
+        {
+            var diamondB = diamond.Of("B");
+            diamondB.Should().ContainInOrder("_A_",
+                                             "B_B",
+                                             "_A_");
+        }
+
+        [Fact]
+        public void Diamond_C()
+        {
+            var diamondC = diamond.Of("C");
+            diamondC.Should().ContainInOrder("__A__",
+                                             "_B_B_",
+                                             "C___C",
+                                             "_B_B_",
+                                             "__A__");
+        }
+
+        [Fact(Skip = "Pending Diamond_C")]
+        public void Diamond_D()
+        {
+            var diamondD = diamond.Of("D");
+            diamondD.Should().ContainInOrder("___A___",
+                                             "__B_B__",
+                                             "_C___C_",
+                                             "D_____D",
+                                             "_C___C_",
+                                             "__B_B__",
+                                             "___A___");
+        }
+    }
+}
